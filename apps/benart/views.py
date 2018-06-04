18bf39747a29b335369366a26973715a4f161782
Anthony <anthonycnoble@live.com>
2018-06-04 11:06:00 -0700

    Condensed image models into one created comment model for future change

--- apps/benart/views.py
+++ apps/benart/views.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.shortcuts import render, redirect
-from models import images, drawings, paintings, gameArt, art3d
+from models import images
 # Create your views here.
 def main(request):
     context = {
         'images': images.objects.all(),
-        'string': "Interested in spesific types of art? use the art dropdown menu to filter results."
+        'string': "Interested in spesific types of art? use the art dropdown menu to filter results.",
+        'type': 0
     }
     return render(request, 'benart/main.html', context)
 
@@ -16,26 +17,30 @@ def about(request):
     return render(request, 'benart/aboutme.html')
 def paint(request):
     context = {
-        'images': paintings.objects.all(),
-        'string': "Paintings"
+        'images': images.objects.all(),
+        'string': "Paintings",
+        'type': 1
     }
     return render(request, "benart/main.html", context)
 def draw(request):
     context = {
-        'images': drawings.objects.all(),
-        'string': "Drawings"
+        'images': images.objects.all(),
+        'string': "Drawings",
+        'type': 2
     }
     return render(request, "benart/main.html", context)
 def threed(request):
     context = {
-        'images': art3d.objects.all(),
-        'string': "3d Art"
+        'images': images.objects.all(),
+        'string': "3d Art",
+        'type': 3
     }
     return render(request, "benart/main.html", context)
 def game(request):
     context = {
-        'images': gameArt.objects.all(),
-        'string': "Game Art"
+        'images': images.objects.all(),
+        'string': "Game Art",
+        'type': 4
     }
     return render(request, "benart/main.html", context)
 def login(request):
